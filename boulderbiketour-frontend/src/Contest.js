e305114bb7b510f1506b1a140f90cd1c5142b4d9
Daniel Kukula <daniel.kuku@gmail.com>
2018-02-09 02:54:56 +0000

    slogans for draft

--- boulderbiketour-frontend/src/Contest.js
+++ boulderbiketour-frontend/src/Contest.js
@@ -1,11 +1,29 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-import React, { Component } from 'react';
+export default class Contest extends React.Component {
+  render() {
+    return (
+      <Form>
 
-export default class Contest extends Component {
-  render(){
-    return(
-      <p>
-      asassasaasa
-      </p>
-    ) 
-  }}
+        <FormGroup>
+          <Label for="first">First Name</Label>
+          <Input type="" name="first" id="first" placeholder="Jon" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="last">Email</Label>
+          <Input type="" name="last" id="last" placeholder="Snow" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Email">Email</Label>
+          <Input type="email" name="email" id="Email" placeholder="email@em.il" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="slogan">Enter your slogan</Label>
+          <Input type="textarea" name="slogan" id="slogan" />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
+}
