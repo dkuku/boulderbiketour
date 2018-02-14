4f1362b5b004eaf2d7c92da272d06b7a23a6083b
Daniel Kukula <daniel.kuku@gmail.com>
2018-02-14 01:01:41 +0000

    some simple tests

--- boulderbiketour-api/test/controllers/riders_controller_test.rb
+++ boulderbiketour-api/test/controllers/riders_controller_test.rb
@@ -1,9 +1,37 @@
 require 'test_helper'
 
 class RidersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get riders_index_url
+  test "test index" do
+    get "/api/v1/riders"
     assert_response :success
   end
 
+  test "test reading from database" do
+    @rider1 = Rider.first
+    puts @rider1.id
+    assert_equal(1 , Rider.count)
+    assert_equal("First1", @rider1.first)
+    assert_equal("Last1", @rider1.last)
+    assert_equal("City1", @rider1.city)
+    assert_equal("ST1", @rider1.state)
+    assert_equal(9.99, @rider1.lat)
+    assert_equal(8.88, @rider1.lng)
+  end
+  
+#  test "should create rider" do
+#  assert_difference('Rider.count') do
+#  post "/api/v1/riders", params: { rider: { first: 'First2', last: 'Last2', city: 'City2', state: 'ST2', lat: 1.11, lng: 2.22 } }
+#  end
+#
+#  @rider2 = Rider.last
+#  assert_equal(2, Rider.count)
+#  assert_equal("First2", @rider2.first)
+#  assert_equal("Last2", @rider2.last)
+#  assert_equal("City2", @rider2.city)
+#  assert_equal("ST2" , @rider2.state)
+#  assert_equal(1.11, @rider2.lat)
+#  assert_equal(2.22, @rider2.lng)
+#  #assert_redirected_to article_path(Article.last)
+#  #assert_equal 'Article was successfully created.', flash[:notice]
+#end
 end
--- boulderbiketour-api/test/controllers/slogans_controller_test.rb
+++ boulderbiketour-api/test/controllers/slogans_controller_test.rb
@@ -1,9 +1,33 @@
 require 'test_helper'
 
 class SlogansControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get slogans_index_url
+  test "test index" do
+    get "/api/v1/slogans"
     assert_response :success
   end
 
+  test "test reading from database" do
+    @slogan = Slogan.first
+    puts @slogan.id
+    assert_equal(1 , Slogan.count)
+    assert_equal("First1" , @slogan.first)
+    assert_equal("Last1" , @slogan.last)
+    assert_equal("em@ai.l" , @slogan.email)
+    assert_equal("12.345.qwe.rt" , @slogan.slogan)
+  end
+  
+  test "should create slogan" do
+  assert_difference('Slogan.count') do
+    post "/api/v1/slogans", params: { slogan: { first: 'First2', last: 'Last2', email: 'email@em.ail', slogan: 'test slogan' } }
+  end
+
+  @slogan = Slogan.last
+  assert_equal(2 , Slogan.count)
+  assert_equal("First2", @slogan.first)
+  assert_equal("Last2", @slogan.last)
+  assert_equal("email@em.ail", @slogan.email)
+  assert_equal("test slogan", @slogan.slogan)
+  #assert_redirected_to article_path(Article.last)
+  #assert_equal 'Article was successfully created.', flash[:notice]
+end
 end
