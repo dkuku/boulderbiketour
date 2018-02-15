d995e7bcafa69e0ccb93bd966768d8e9427ac0a3
Daniel Kukula <daniel.kuku@gmail.com>
2018-02-15 22:25:17 +0000

    working map with markers

--- boulderbiketour-frontend/src/Location.js
+++ boulderbiketour-frontend/src/Location.js
@@ -1,14 +1,91 @@
-
 import React, { Component } from 'react';
+const fetch = require("isomorphic-fetch");
+const { compose, withProps, withHandlers, withStateHandlers } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} = require("react-google-maps");
+const { InfoBox } = require("react-google-maps/lib/components/addons/InfoBox");
+const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
 
-export default class Location extends Component {
-  render(){
-    return(
-      <div>
-AIzaSyCIAWHNxZM9J5lty-TuwcuTEOpf0iTsGGk
-      </div>
-    ) 
+const MapWithAMarkerClusterer = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAl1BhA3YpByfJQ4DImZfzcV16VxZr_1JI&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withHandlers({
+    onMarkerClustererClick: () => (markerClusterer) => {
+      const clickedMarkers = markerClusterer.getMarkers()
+      console.log(`Current clicked markers length: ${clickedMarkers.length}`)
+    },
+  }),
+    withStateHandlers(() => ({
+    isOpen: false,
+  }), {
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={11}
+    defaultCenter={{ lat: 40, lng: -105.3 }}
+  >
+    <MarkerClusterer
+      onClick={props.onMarkerClustererClick}
+      averageCenter
+      enableRetinaIcons
+      gridSize={60}
+    >{console.log(props)}
+      {props.markers.map(marker => (
+        <Marker
+          key={marker.id}
+          position={{ lat: Number(marker.lat), 
+                      lng: Number(marker.lng) }}
+          onClick={props.onToggleOpen}>
+
+      {props.isOpen && <InfoBox
+        onCloseClick={props.onToggleOpen}
+        options={{ closeBoxURL: ``, enableEventPropagation: true }}
+      >
+        <div style={{ backgroundColor: `yellow`, opacity: 0.75, padding: `12px` }}>
+          <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
+        {marker.first.concat(" ", marker.last)}
+          </div>
+        </div>
+      </InfoBox>}
+       </Marker>
+      ))}
+    </MarkerClusterer>
+  </GoogleMap>
+);
+
+export default class Location extends React.PureComponent {
+  componentWillMount() {
+    this.setState({ markers: [] })
   }
 
+  componentDidMount() {
+    const url = 'http://localhost:3001/api/v1/riders.json'
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ markers: data.riders });
+      });
+  }
+
+  render() {
+    return (
+      <MapWithAMarkerClusterer markers={this.state.markers} />
+    )
+  }
 }
+
 
