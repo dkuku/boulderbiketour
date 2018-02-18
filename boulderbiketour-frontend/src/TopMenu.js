4fd33d1cbc6ff9f023c227aa6527a3281797dc2e
Daniel Kukula <daniel.kuku@gmail.com>
2018-02-18 08:57:16 -0500

    page title and dark navbar

--- boulderbiketour-frontend/src/TopMenu.js
+++ boulderbiketour-frontend/src/TopMenu.js
@@ -1,52 +1,56 @@
-
 import React, { Component } from 'react';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
 } from 'reactstrap';
 const instaAddress = "https://api.instagram.com/oauth/authorize/?client_id=ea86d2a774bc4c979110dbf1c016e44e&redirect_uri=http://212.8.251.135:3000/Photos&response_type=token"
 
 export default class TopMenu extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render(){
-    return(
-      <Navbar color="faded" light expand="md">
-      <NavbarBrand href="/">Bike Boulder Tour</NavbarBrand>
-      <NavbarToggler onClick={this.toggle} />
-      <Collapse isOpen={this.state.isOpen} navbar>
-      <Nav className="ml-auto" navbar>
-      <NavItem>
-      <NavLink href={instaAddress}>Photos</NavLink>
-      </NavItem>
-      <NavItem>
-      <NavLink href="/Location">Location</NavLink>
-      </NavItem>
-      <NavItem>
-      <NavLink href="/Riders">Riders</NavLink>
-      </NavItem>
-      <NavItem>
-      <NavLink href="/Contest">Contest</NavLink>
-      </NavItem>
-      </Nav>
-      </Collapse>
-      </Navbar>
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render(){
+        return(
+            <Navbar className="bg-dark navbar-dark" dark expand="md">
+                <Container>    
+                <NavbarToggler onClick={this.toggle} />
+            <Collapse className="text-center justify-content-center" isOpen={this.state.isOpen} navbar>
+                <Nav navbar>
+            <NavItem>
+                <NavLink href="/">BikeBoulderTour</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href={instaAddress}>Photos</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="/Location">Location</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="/Riders">Riders</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="/Contest">Contest</NavLink>
+            </NavItem>
+            </Nav>
+        </Collapse>
+    </Container>
+    </Navbar>
     ) 
-  }
-}
+    }
+    }
