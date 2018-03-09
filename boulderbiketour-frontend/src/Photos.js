5c57d8d465f92041d257218b29c8e2aea998c293
Daniel Kukula <daniel.kuku@gmail.com>
2018-03-09 15:59:08 -0500

    query string for flickr URI encoding

--- boulderbiketour-frontend/src/Photos.js
+++ boulderbiketour-frontend/src/Photos.js
@@ -1,46 +1,55 @@
 
 import React, { Component } from 'react';
 import axios from 'axios';
+import queryString from 'query-string';
 import Image from './components/Image';
-import ImageCarousel from './components/ImageCarousel';
 
-const queryString = require('query-string');
-const parsedHash = queryString.parse(window.location.hash);
+
 
 export default class Photos extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            data: []
-        }
+            this.state = {
+                data: []
+            }
     }
-    flickrURL(){
-        return 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=ca3783111609d69139840916b7a01ad2&format=json&nojsoncallback=1&per_page=5'
+
+    flickrURL(page=1){
+        const address= "https://api.flickr.com/services/rest/?"
+        return address + queryString.stringify({
+            page: page,
+            method: "flickr.photos.search",
+            tags: "bikerace,boulderbiketour",
+            api_key: 'ca3783111609d69139840916b7a01ad2',
+            format: 'json',
+            nojsoncallback: 1,
+            per_page: 5
+        })
     }
 
     imageURL(item) {
-            return 'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '.jpg'
+        return 'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '.jpg'
     }
 
     componentDidMount() {
         axios.get(this.flickrURL())
             .then(response => {
-                this.setState({data: response.data.photos.photo});
-            })
-            .catch(error => console.log(error))
+                    this.setState({data: response.data.photos.photo});
+                    })
+        .catch(error => console.log(error))
     }
 
 
-    render(){
-        return (
+render(){
+    return (
             <div>
-                { this.state.data.map( (data) => {
-                        console.log(this.imageURL(data))
-                        return (
+            { this.state.data.map( (data) => {
+                    console.log(this.imageURL(data))
+                    return (
                             <Image url={this.imageURL(data)} key={data.id} />
-                        )}
-                )}
+                           )}
+                    )}
             </div>
-        )
-    }
+           )
+}
 }
